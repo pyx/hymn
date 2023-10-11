c30857f67d4475a02efe398baaf54c43bd618b95
Philip Xu <pyx@xrefactor.com>
2023-10-11 08:35:07 -0400

    Updated docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 #
 # Hymn documentation build configuration file
-import os
+import pathlib
 import sys
-
-PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
-sys.path.insert(0, PROJECT_DIR)
-import hymn
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
 ]
 
@@ -19,24 +15,16 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 project = u'Hymn'
-copyright = u'2014-2020, Philip Xu'
+copyright = u'2014-2023, Philip Xu'
 author = u'Philip Xu'
 
-version = '%d.%d' % hymn.__version__
-release = hymn.VERSION
-
-language = None
+release = "1.0.0"
 
 exclude_patterns = ['_build']
 
 pygments_style = 'colorful'
 
-todo_include_todos = False
-
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:
-    html_theme = 'bizstyle'
+html_theme = 'bizstyle'
 
 htmlhelp_basename = 'Hymndoc'
 
@@ -52,6 +40,6 @@ man_pages = [
 
 texinfo_documents = [
   (master_doc, 'Hymn', u'Hymn Documentation',
-   author, 'Hymn', hymn.__doc__,
+   author, 'Hymn', "Hy Monad Notation - a monad library for Hy",
    'Miscellaneous'),
 ]
